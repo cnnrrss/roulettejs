4bc5181f9c5f96827846e009ea6d53df3204f14e
Connor Vanderhook <connorvhook@gmail.com>
2018-02-17 10:34:21 -0800

    feat: wire up the button for liftoff

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,16 +4,24 @@ export type InputProps = {
     className?: string,
     onChange?: EventHandler<ChangeEvent<HTMLInputElement>>,
     defaultValue?: string,
-    placeholder?: string
+    placeholder?: string,
+    query?: string
 } & typeof defaultProps;
 
 const defaultProps = {
     className: 'search-input',
-    placeholder: 'Endless frameworks await..'
+    placeholder: 'Endless frameworks await..',
+    query: 'framework+NOT+\"hello+world\"+NOT+\"awesome\"+NOT+\"todo\"+language:javascript'
 };
+
+
 
 export class Input extends Component<InputProps> {
     static defaultProps = defaultProps;
+
+    handleChange(event) {
+        this.setState({ query: event.target.value + '+' + this.props.query})
+    }
 
     render() {
         const {
@@ -23,7 +31,7 @@ export class Input extends Component<InputProps> {
             ...otherProps
         } = this.props;
         return (
-            <input className={className} placeholder={placeholder} />
+            <input className={className} placeholder={placeholder} onChange={this.handleChange.bind(this)} />
         )
     }
 }
